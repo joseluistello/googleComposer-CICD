be201edf729657ab5a27c340ce5553bce092d32b
joseluistello <jltello96@gmail.com>
2023-11-02 20:18:37 -0600

    prueba7

--- dags/data_to_google_test.py
+++ dags/data_to_google_test.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, ANY
 import internal_unit_testing
 from dags import data_to_google
 
@@ -13,12 +13,12 @@ class TestStockDataDag(unittest.TestCase):
         self.assertIsNotNone(task)
         self.assertEqual(task.task_id, 'move_data_from_sheet_to_gcs')
 
-    @patch("data_to_google.storage.Client")
-    @patch("data_to_google.gspread.authorize")
+    @patch("dags.data_to_google.storage.Client")
+    @patch("dags.data_to_google.gspread.authorize")
     def test_move_data_function(self, mock_gspread_authorize, mock_storage_client):
         # Configura el mock para devolver una cadena JSON válida
         mock_blob = Mock()
-        mock_credentials_as_string = '{"type": "service_account", "project_id": "your_project_id"}'  # Un ejemplo de JSON de credenciales
+        mock_credentials_as_string = '{"type": "service_account", "project_id": "your_project_id"}'
         mock_blob.download_as_string.return_value = mock_credentials_as_string
 
         # Configura el mock del bucket para devolver el mock_blob cuando se llame a blob()
@@ -28,11 +28,20 @@ class TestStockDataDag(unittest.TestCase):
         # Configura el mock del cliente de almacenamiento para devolver el mock_bucket cuando se llame a bucket()
         mock_storage_client.return_value.bucket.return_value = mock_bucket
 
-        # Ahora puedes llamar a la función que estás probando
-        result = data_to_google.move_data_from_sheet_to_gcs()
+        # Llama a la función que estás probando
+        data_to_google.move_data_from_sheet_to_gcs()
 
-        # Realiza tus aserciones aquí
-        # self.assertEqual(result, expected_result)  # Asegúrate de definir 'expected_result'
+        # Verifica que se llamó al método upload_from_string del blob con los argumentos correctos
+        mock_blob.upload_from_string.assert_called_once_with(ANY, content_type='text/csv')
+
+        # Verifica que se llamó al método authorize de gspread con las credenciales correctas
+        mock_gspread_authorize.assert_called_once()
+
+        # Verifica que se obtuvo el bucket correcto
+        mock_storage_client.return_value.bucket.assert_called_once_with('credentials-buckket')
+
+        # Verifica que se obtuvo el blob correcto
+        mock_bucket.blob.assert_called_once_with('credentials.json')
 
 if __name__ == '__main__':
     unittest.main()
