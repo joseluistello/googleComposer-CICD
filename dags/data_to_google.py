5e18c39f64ab4b23258ef8db3a36835dcf7f0607
joseluistello <jltello96@gmail.com>
2023-11-03 03:24:23 -0600

    prueba9

--- dags/data_to_google.py
+++ dags/data_to_google.py
@@ -3,51 +3,65 @@ from google.oauth2.service_account import Credentials
 from google.cloud import storage
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 import csv
 import io
 import json
+
+SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive'
+]
 
 def get_credentials_from_gcs(bucket_name, credentials_path):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(credentials_path)
     credentials_info = json.loads(blob.download_as_string())
-    credentials = Credentials.from_service_account_info(credentials_info)
+    credentials = Credentials.from_service_account_info(credentials_info, scopes=SCOPES)
     return credentials
 
-def move_data_from_sheet_to_gcs():
-    # Configura la autenticación para Google Sheets y GCS
+def move_data_from_sheet_to_gcs(**kwargs):
     creds = get_credentials_from_gcs('credentials-buckket', 'credentials.json')
     client = gspread.authorize(creds)
 
-    # Abre el Google Sheet y lee los datos
     spreadsheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1JLXYJgWp5knCfIicIb6qrgLJxSGNPZZIezqwBUxoysk')
     sheet = spreadsheet.get_worksheet(0)
     data = sheet.get_all_values()
 
-    # Escribe los datos en un flujo de bytes en memoria
     in_memory_csv = io.StringIO()
     writer = csv.writer(in_memory_csv)
     writer.writerows(data)
     in_memory_csv.seek(0)
-
-    # Carga el archivo en GCS
+    
     storage_client = storage.Client()
     bucket = storage_client.bucket('bronze_layer')
-    blob = bucket.blob('data/temp_data.csv')
+    blob = bucket.blob('data/db_bronze_layer.csv')
     blob.upload_from_string(in_memory_csv.getvalue(), content_type='text/csv')
 
 default_args = {
     'owner': 'you',
     'start_date': datetime(2023, 11, 2),
     'retries': 1,
+    'retry_delay': timedelta(minutes=5), 
+    'catchup': False
 }
 
-dag = DAG('move_data_dag', default_args=default_args, schedule_interval='@daily')
+dag = DAG(
+    'data_swag_2',
+    default_args=default_args,
+    description='DAG para mover datos de Google Sheets a GCS',
+    schedule_interval='@daily',
+)
 
 move_data_task = PythonOperator(
     task_id='move_data_from_sheet_to_gcs',
     python_callable=move_data_from_sheet_to_gcs,
-    dag=dag
+    provide_context=True,
+    dag=dag,
 )
+
+if __name__ == "__main__":
+    dag.clear(reset_dag_runs=True)
+    dag.run()
